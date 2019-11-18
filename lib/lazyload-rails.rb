d3773ff28e2393995e119a8f01f5bbe25d36da9a
keithbooher <keith.booher@yahoo.com>
2019-11-18 17:18:08 -0600

    lazy_class config added

--- lib/lazyload-rails.rb
+++ lib/lazyload-rails.rb
@@ -49,11 +49,16 @@ ActionView::Helpers::AssetTagHelper.module_eval do
   private
 
   def to_lazy(image_html)
+
     img = Nokogiri::HTML::DocumentFragment.parse(image_html).at_css("img")
 
     img["data-original"] = img["src"]
     img["src"] = Lazyload::Rails.configuration.placeholder
 
+    if Lazyload::Rails.configuration.lazy_class
+      img["class"] = img["class"].present? ? img["class"].to_s + " " + Lazyload::Rails.configuration.lazy_class : Lazyload::Rails.configuration.lazy_class 
+    end
+    
     img.to_s.html_safe
   end
 
